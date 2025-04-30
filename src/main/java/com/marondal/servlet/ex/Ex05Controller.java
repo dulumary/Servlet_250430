926b958a4e8855ebdc27ca4da2a9c14b16d499db
dulumary <lecture@hagulu.com>
2025-04-30 18:45:36 +0900

    [김인규]  - post method 일부 예제 작성

--- src/main/java/com/marondal/servlet/ex/Ex05Controller.java
+++ src/main/java/com/marondal/servlet/ex/Ex05Controller.java
@@ -6,8 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Ex05Controller extends HttpServlet {
 	
+	// 파라미터가 
+	// 보안에 민감한 데이터가 
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+		
+		// 이름과 생년월일을 전달 받고,
+		// 이름과 나이를 html로 구성
+		
 		
 	}
 
